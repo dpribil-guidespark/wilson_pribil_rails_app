f5cd38ffa64ac602836a6d1692d4ba8beb5a7966
David Wilson <dwilson@guidespark.com>
2014-09-05 10:45:36 -0700

    Basic Back-End #5 - Save names with capitalization for easy searching

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :guesses
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  before_save {self.first_name = first_name.capitalize}
+  before_save {self.last_name = last_name.capitalize}
+
 end

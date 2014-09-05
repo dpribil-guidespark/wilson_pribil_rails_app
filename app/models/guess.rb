36c08b11aa9a00115ef21e30e78b38021add7b2e
David Wilson <dwilson@guidespark.com>
2014-09-05 11:01:27 -0700

    Basic Back-End #5 - Find the challenge ID before saving

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -4,4 +4,16 @@ class Guess < ActiveRecord::Base
   validates :challenge_id, presence: true
   validates :user_id, presence: true
   validates :answer, presence: true
+
+  before_save do
+    #find challenge_id
+    current_time = self.created_at
+    last_challenge = Challenge.where(created_at: current_time-(7.day)..current_time)
+    self.challenge_id = last_challenge[:id]
+
+    #find user_id
+
+
+  end
+
 end

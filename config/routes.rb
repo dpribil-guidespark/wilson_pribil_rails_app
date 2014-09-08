93e4457a0c6c0ca4b0c089286243f449df4d3e25
David Wilson <dwilson@guidespark.com>
2014-09-08 14:05:40 -0700

    Create nice routes #7 - Adding guesses to a challenge screen and also adding challenges/latest as a route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       resources :guesses
     end
   end
+
+  get '/challenges/latest', to: "challenges#latest_challenge"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

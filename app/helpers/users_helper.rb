d0abd62ce28d9cc9f3c45dfd09f44798978a372a
David Wilson <dwilson@guidespark.com>
2014-09-09 09:30:50 -0700

    Create Leaderboard page #9 - extra commit that was forgotten about in merge conflict

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,11 @@
 module UsersHelper
 
+  def number_of_guesses(user)
+    return user.guesses.count
+  end
+
+  def number_of_wins(user)
+    return user.guesses.where(:status => STATUS_RIGHT).count
+  end
+
 end

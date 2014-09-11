df6d25df92407689b110aaa6a6ad848d29a84245
David Wilson <dwilson@guidespark.com>
2014-09-11 11:42:17 -0700

    S-01674 - add previous and next commands to the Challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,5 +5,13 @@ class Challenge < ActiveRecord::Base
   validates :question, presence: true, :uniqueness => {:scope => :answer}
   validates :answer,   presence: true
 
+  def previous
+    Challenge.where('challenges.id < ?', self.id).last
+  end
+
+  def next
+    Challenge.where('challenges.id > ?', self.id).first
+  end  
+
 
 end

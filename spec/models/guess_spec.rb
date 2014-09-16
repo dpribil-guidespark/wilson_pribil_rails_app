83dd87abb30c5955c8466d5deeae1b4d012eb65b
David Wilson <dwilson@guidespark.com>
2014-09-16 14:02:37 -0700

    Few more tests

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -7,17 +7,7 @@ describe Guess do
   subject { @guess }
 
   it { should respond_to(:answer) }
-
-  describe "when saving should fill in fields" do
-    before { @guess.save }
-    it { should respond_to(:challenge_id)}
-    it { should respond_to(:user_id)}
-    it { should be_valid }
-  end
-
-  describe "when first created status should be 0" do
-    it { should respond_to(:status)}
-    its(:status) { should eq STATUS_WRONG}
-  end
+  it { should respond_to(:challenge_id)}
+  it { should respond_to(:user_id)}
 
 end
